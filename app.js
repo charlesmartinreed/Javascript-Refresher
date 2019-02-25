48ee6f60f401b9bc4cf9da954e6f0e5f27fd7b18
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-25 17:45:56 -0600

    Basics of the JS console.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+// A few cool console tricks
+var greeting = 'Yo';
+console.log(greeting);
+console.table({a:1, b:2});
+console.error('This is an error!');
+console.warn('This is a warning!');
 
+//console.time displays the time it takes to execute a script
+console.time('Hello');
+	console.log('Hello world');
+	console.log('Hello world');
+	console.log('Hello world');
+	console.log('Hello world');
+	console.log('Hello world');
+	console.log('Hello world');
+console.timeEnd('Hello');
+
+//console.clear();

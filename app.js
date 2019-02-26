3a8d735aff630c7f789152b8bf5985795baf7c92
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-25 18:00:42 -0600

    Basics of var, let and const in JS.

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
-// A few cool console tricks
-var greeting = 'Yo';
+//var, let and const
+
+// var name = 'Jane Doe';
+// console.log(name); //prints Jane Doe in the console
+//
+// name = 'Steve Smith';
+// console.log(name); //prints Steve Smith in the console
+
+// var names can only include letters, numbers, _, and $.
+// Can't begin with numbers
+var greeting;
+console.log(greeting); // "undefined"
+greeting = 'Hello';
 console.log(greeting);
-console.table({a:1, b:2});
-console.error('This is an error!');
-console.warn('This is a warning!');
 
-//console.time displays the time it takes to execute a script
-console.time('Hello');
-	console.log('Hello world');
-	console.log('Hello world');
-	console.log('Hello world');
-	console.log('Hello world');
-	console.log('Hello world');
-	console.log('Hello world');
-console.timeEnd('Hello');
+//Multi word vars
+var firstName = 'John'; //camel case tends to be preferred
+var first_name = 'Sara'; //snake case
+var FirstName = 'Tom'; //Pascal case - classes use this convention.
 
-//console.clear();
+//LET works similarly to VAR
+let name = 'Jane Doe';
+console.log(name); //prints Jane Doe in the console
+
+name = 'Steve Smith';
+console.log(name); //prints Steve Smith in the console
+
+//CONST, in this case, can't be changed after initialization
+// const name = 'Jason';
+// console.log('name');
+
+//also have to assign a value for const AT initialization
+//const greeting; //this is invalid, missing declaration error
+
+const person = {
+	name: 'John',
+	age: 30
+}
+
+console.log(person);
+
+//you can, however, change the data INSIDE the const object.
+person.name = 'Sara';
+console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+//number = [2, 3, 4, 5, 6]; //can't be done. Reassign not allowed.
+
+console.log(numbers);

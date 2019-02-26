68a9781c61b151092b712ea3d03c6ac075e4503c
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-26 16:00:47 -0600

    Basic demonstration of mouse events.

--- app.js
+++ app.js
@@ -1,41 +1,24 @@
-// We interact with our web page through event listeners
+// MORE ON MOUSE EVENTS
 
-// Adding an event listener to the 'clear task' button
-// addEventListener takes the event to listen for and the anonymous function that describes what should occur when that event is detected
+const clearBtn = document.querySelector('.clear-tasks');
+const card = document.querySelector('.card');
+const heading = document.querySelector('h5');
 
-document.querySelector('.clear-tasks').addEventListener('click', (e) => {
-	//e is the param name for the event object, we're using it to prevent the default behavior for this link, i.e,. stop it from redirecting
-	//e.preventDefault();
-})
+//clearBtn.addEventListener('click', runEvent);
+//clearBtn.addEventListener('dblclick', runEvent);
+// clearBtn.addEventListener('mousedown', runEvent); //click and hold
+//clearBtn.addEventListener('mouseup', runEvent);
 
-// triggering named functions
-document.querySelector('.clear-tasks').addEventListener('click', onClick);
+// card.addEventListener('mouseenter', runEvent); //when the mouse enters the element's bounds
+// card.addEventListener('mouseleave', runEvent);
+// card.addEventListener('mouseover', runEvent);
+// card.addEventListener('mouseout', runEvent);
 
-function onClick(e){
-	let val;
-	val = e; //has many different properties
+card.addEventListener('mousemove', runEvent); //any movement, all movement, inside that element
 
-	//event target element
-	val = e.target; //this is the element that was clicked on
-	val = e.target.className;
-	val = e.target.classList;
-	// e.target.innerText = 'Hello';
-
-	//anything you can do with the DOM outside of an event can also be done INSIDE of an event.
-
-	//check the event type
-	val = e.type;
-
-	//get the timestamp
-	val = e.timeStamp;
-
-	//get coords relative to the window
-	val = e.clientY; //from the top of the window to where the click was registered on the element.
-	val = e.clientX;
-
-	//get coords relative to element itself
-	val = e.offsetY;
-	val = e.offsetX;
-
-	console.log(val);
+// EVENT HANDLER FUNCTION
+function runEvent(e) {
+	console.log(`EVENT TYPE: ${e.type}`);
+	heading.innerText = `X: ${e.offsetX}, Y: ${e.offsetY}`; //this shows our coordinates
+	document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`
 }

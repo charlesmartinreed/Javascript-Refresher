1eeba877f622877245b2d8c50baf40ac4976ea31
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-26 13:19:48 -0600

    Basic demonstration of DOM selectors for single DOM elements.

--- app.js
+++ app.js
@@ -1,46 +1,39 @@
-//Document Object Model
+// DOM SELECTORS allows us to directly access elements from the DOM
+// This also us to do much of the work previously handled by jQuery
 
-// Tree of nodes (elements like h1, a, p, etc...) created by the browser
-// Javascript can e used to read/write/manipulate to the DOM
-// the DOM is object-oriented, meaning the page is represented by a series of objects that themselves can offer properties and methods.
+// Single element selectors let you grab an element by class or id. Only stores one thing, so if you target a class, it'll only grab the first element that uses that class.
 
-let val;
+// Multiple element selectors can be used to return HTML Collections comprising the full array of items that use an id or class
 
-val = document; //gives us the entire document, anything and everything in the html
+//document.getElementById();
+const taskTitle = document.getElementById('task-title');
+console.log(taskTitle); //shows the h5 in index.html
 
-val = document.all; //HTML COLLECTION of tag elements. This is zero based, like an array.
-val = document.all[3]; //gives us the 4th
-val = document.all.length; //count of elements in the dom
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+//grab things from the element
+console.log(taskTitle.id);
+console.log(taskTitle.className);
 
-// Though it isn't recommended, you can grab reference to html elements by referring to their semantic name
-val = document.forms;
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+//we can also change styling by targeting
+//anythign you can do in CSS, can be done via this method
+taskTitle.style.background = '#333';
+taskTitle.style.color = '#FFF';
+taskTitle.style.padding = '5px';
+//document.getElementById('task-title').style.display = 'none';
 
-val = document.links; //HTML COLLECTION of the links in current DOM
-val = document.links[0];
-val = document.links[0].className;
-val = document.links[0].classList[0];
+//you can also change the content by changing the content or even the HTML
+taskTitle.textContent = 'Task List!';
+taskTitle.innerText = 'My Tasks';
+taskTitle.innerHTML = '<span style="color: orange">Task List</span>'
 
-val = document.images; //this will be empty for us, of course
+//document.querySelector();
+// takes any CSS selector
+console.log(document.querySelector('#task-title')); //by id
+console.log(document.querySelector('.card-title')); //by class
+console.log(document.querySelector('h5')); //the first h5 on the page
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src'); //app.js
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue'; //you can also nest elements, as in CSS
 
-//HTML COLLECTIONS CAN BE TURNED INTO ARRAYS
-//THIS ALLOWS US TO USE ARRAY CLASS METHODS ON THEM
-let scripts = document.scripts;
-let scriptsArray = Array.from(scripts);
-
-scriptsArray.forEach(function(script) {
-	console.log(script);
-});
-console.log(val);
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'purple';
+document.querySelector('li:nth-child(4)').textContent = 'Changed with Query Selector!';

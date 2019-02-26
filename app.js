6a3daad18317ae619295bc0c86d9f81ce8116266
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-26 00:13:24 -0600

    Basics of Object Literals in JS.

--- app.js
+++ app.js
@@ -1,72 +1,35 @@
-//A LITTLE BIT ABOUT ARRAYS
-
-// MAKING A BASIC ARRAY
-const numbers = [43, 56, 23, 99, 82, 15];
-const numbers2 = new Array(295, 285, 9538, 312, 11);
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
-
-//heterogenerous arrays are possible in JS as well
-const mixed = [22, 'Hello', undefined, null, {a:1, b:1}, new Date()];
+//DEFINING AN OBJECT LITERAL
+const person = {
+	firstName: 'Steve',
+	lastName: 'Wilitis',
+	age: 59,
+	email: 'steve@msn.com',
+	hobbies: ['music', 'sports'],
+	address: {
+		city: 'miami',
+		state: 'FL'
+	},
+	getBirthYear: function() {
+		return 2019 - this.age; //this pertains to the object itself
+	}
+}
 
 let val;
+val = person.firstName; //or person[firstName]
+val = person.hobbies[1]; //sports
+val = person.address.state; //FL
+val = person.address['city']; //miami
+val = person.getBirthYear();
 
-//GET THE ARRAY LENGTH
-val = numbers.length; //6
+console.log(val);
 
-//CHECK FOR ARRAY
-val = Array.isArray(fruit); //true
+const people = [
+	{name: 'John', age: 30},
+	{name: 'Mike', age: 23},
+	{name: 'Barry', age: 17},
+	{name: 'Debra', age: 41}
+];
 
-//GRAB SINGLE VALUE FROM ARRAY
-val = numbers[3]; //99
-
-// INSERT INTO AN ARRAY
-numbers[2] = 100;
-
-// FIND INDEX OF A VALUE
-val = numbers.indexOf(44); //-1, not in the array
-
-//MUTATING ARRAYS
-
-//PUSH adds to the END OF AN ARrAY
-//UNSHIFT ADDS TO THE FRONT OF AN ARRAY
-//POP REMOVES THE LAST OF AN ARRAY
-//SHIFT REMOVES THE FIRST OF AN ARRAY
-
-// numbers.push(300); //adds 300
-// numbers.unshift(55); //adds 55
-// numbers.pop(); //removes 300
-// numbers.shift(); //removes 300
-
-//CUTTING FROM ARRAYS
-// numbers.splice(1, 1); //start at index 1, end at index 1
-// numbers.splice(1, 3); //start at index 1, end at 3
-//
-// numbers.reverse();
-
-//CONCATENATE TWO ARRAYS
-val = numbers.concat(numbers2);
-
-//SORTING ARRAYS
-// val = fruit.sort();
-// val = numbers.sort(); //only compares first digit
-
-//to sort according to numeric value, pass a callback into sort method
-// val = numbers.sort(function(x, y) {
-// 	return x - y;
-// })
-//
-// val = numbers.sort(function(a, b) {
-// 	return b - a;
-// })
-
-//FINDING ELEMENTS IN ARRAY
-function under50(number) {
-	return number < 50
-} //returns the first number under 50 in the array
-
-val = numbers.find(under50);
-
-val = numbers.find((num) => { return num > 50} )
-
-console.log(numbers)
-console.log(val)
+for (var i=0; i < people.length; i++) {
+	console.log(people[i].name); //prints John, Mike, Barry, Debra
+}

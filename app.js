3a99b894e8466245d2702ab9b700bcc784d3147a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-26 02:09:37 -0600

    Basics of scope in JS.

--- app.js
+++ app.js
@@ -1,58 +1,34 @@
-// WORKING WITH THE DOM
+// GLOBAL SCOPE
+var a = 1;
+let b = 2;
+const c = 3;
 
-// WINDOW OBJECT in the browser is the user environment.
-// Document object exists on the window as a property.
-// Methods like console.log or alert exists on the window
-
-// ALERT
-//window.alert('Hello world'); //usually see this as alert();
-
-// PROMPT
-//const input = prompt(); //provides text input
-//alert(input); //throws up an alert with the contents of our prompt call, i.e, what was typed
-
-// CONFIRM - provides a OK/CANCEL selection box to the user
-// if(confirm('Are you sure?')) {
-// 	console.log('YES');
-// } else {
-// 	console.log('NO');
+//these are separate variables because they're in a different scope
+// function test() {
+// 	var a = 4;
+// 	let b = 5;
+// 	const c = 6;
+// 	console.log('Function scope: ', a, b, c);
 // }
+//
+// test()
+//
+// console.log('Global scope: ', a, b, c);
 
-//PROPERTIES
+//block level scope involves anything in a if-statement, for-loop, etc. Anything in { } basically
+if(true) {
+	// Block Scope
+	var a = 4; //note that this is hoisted up from the block scope to the variable scope
+	let b = 5;
+	const c = 6;
+	console.log('Block scope', a, b, c); //4, 5, 6
+}
 
-let val;
-//grab window height and width
-val = window.outerHeight;
-val = window.outerWidth; //from the edges
+console.log('Global scope: ', a, b, c); //4, 2, 3
 
-val = window.innerHeight;
-val = window.innerWidth; //includes the visible content like scroll bars, etc.
+//with let, this stays confined. with var a, it hoists up to the global scope.
+for(let a = 0; a < 10; a++) {
+	console.log(`Loop: ${a}`);
+}
 
-val = window.scrollY; //scroll point location on the current window for the user
-val = window.scrollX; //these are useful for playing animations as a user scrolls
-
-//location objects
-val = window.location; //gives things like the http protocol, origin address, port #, etc
-val = window.location.hostname;
-val = window.location.search; //shows the query string used to reach your page
-
-
-//REDIRECT
-// window.location.href = "https://google.com" //sends you to google when reloading
-//window.location.reload(); //reloads the page, obviously
-
-//HISTORY OBJECT - gets browsing history
-//window.history.go(-1); //sends the browser back one page
-//val = window.history.length //reports the number of sites navigated to before current one
-
-// NAVIGATOR OBJECT - details on the browser itself
-//includes things like the geoLocation, userAgent, platform
-val = window.navigator;
-val = window.navigator.appName;
-val = window.navigator.appVersion;
-val = window.navigator.userAgent;
-val = window.navigator.platform;
-val = window.navigator.location;
-
-
-console.log(val);
+console.log('Global scope: ', a, b, c);

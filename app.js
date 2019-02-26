f57320b21921651776ea9120e3651b36334f9586
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-26 14:36:25 -0600

    Basic demonstration of creating/appending elements in the DOM using JS.

--- app.js
+++ app.js
@@ -1,59 +1,25 @@
-let val;
+//  CREATING elements
+const li = document.createElement('li');
 
-const list = document.querySelector('.collection');
-const listItem = document.querySelector('li.collection-item:first-child'); //first item
+// Add class or ID
+li.className = 'collection-item';
+li.id = `new-item`;
 
-// looking a different properties attached to these so-called nodes
+// Add any attribute
+li.setAttribute('title', 'New Item');
 
-//get the child nodes of the ul
-val = list.childNodes; //returns node list of all the child nodes, including text nodes
-//line breaks count as text nodes
+// Create text node by appending it
+li.appendChild(document.createTextNode('Hello World'));
 
-// get the children element nodes -
-val = list.chiildren; // returns html collection, just the elements, no text nodes. Generally this is more frequently used that childNodes.
+// Create the new link element
+const link = document.createElement('a');
+link.className = 'delete-item secondary-content'
+link.innerHTML = '<i class="fa fa-remove"</i>'
 
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName; //#text
-val = list.childNodes[0].nodeType; //3 - Text node
+// Append link to li
+li.appendChild(link);
 
-// The numbers returned by nodeType pertain to what type of node they are
-// 1 - element
-// 2 - Attribute (deprecated)
-// 3 - Text node
-// 8 - Comment
-// 9 - Document itself
-// 10 - Doctype
+// Attach it to ul as a child
+document.querySelector('ul.collection').appendChild(li);
 
-val = list.children;
-val = list.children[1].textContent = 'Hello';
-
-// we can also get children of children
-val = list.children[3].children; //our list items have a link in them, which represents our delete button
-list.children[3].children[0].id = 'test-link'; //we can also manipuate children of children
-val = list.children[3].children[0];
-
-
-// first child
-val = list.firstChild; //gives first node, element OR text
-val = list.firstElementChild; // gives first element node, not including text nodes
-
-// last child
-val = list.lastChild;
-val = list.lastElementChild;
-
-//get the child count for elements
-val = list.childElementCount;
-
-//get parent node or parents of parents
-val = listItem.parentNode;
-val = listItem.parentElement;
-val = listItem.parentElement.parentElement;
-
-// get next sibling
-val = listItem.nextSibling; //nodes, including text
-val = listItem.nextElementSibling.nextElementSibling; //nodes, excluding text
-
-val = listItem.previousSibling; //nodes, including text
-val = listItem.previousElementSibling.previousElementSibling; //nodes, excluding text
-
-console.log(val);
+console.log(li);

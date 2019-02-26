364ff0a4ee47025b6ca9cf32a428587d87f8da8d
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-26 13:01:02 -0600

    Basic demonstration of working with the DOM in JS.

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-// GLOBAL SCOPE
-var a = 1;
-let b = 2;
-const c = 3;
+//Document Object Model
 
-//these are separate variables because they're in a different scope
-// function test() {
-// 	var a = 4;
-// 	let b = 5;
-// 	const c = 6;
-// 	console.log('Function scope: ', a, b, c);
-// }
-//
-// test()
-//
-// console.log('Global scope: ', a, b, c);
+// Tree of nodes (elements like h1, a, p, etc...) created by the browser
+// Javascript can e used to read/write/manipulate to the DOM
+// the DOM is object-oriented, meaning the page is represented by a series of objects that themselves can offer properties and methods.
 
-//block level scope involves anything in a if-statement, for-loop, etc. Anything in { } basically
-if(true) {
-	// Block Scope
-	var a = 4; //note that this is hoisted up from the block scope to the variable scope
-	let b = 5;
-	const c = 6;
-	console.log('Block scope', a, b, c); //4, 5, 6
-}
+let val;
 
-console.log('Global scope: ', a, b, c); //4, 2, 3
+val = document; //gives us the entire document, anything and everything in the html
 
-//with let, this stays confined. with var a, it hoists up to the global scope.
-for(let a = 0; a < 10; a++) {
-	console.log(`Loop: ${a}`);
-}
+val = document.all; //HTML COLLECTION of tag elements. This is zero based, like an array.
+val = document.all[3]; //gives us the 4th
+val = document.all.length; //count of elements in the dom
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
 
-console.log('Global scope: ', a, b, c);
+// Though it isn't recommended, you can grab reference to html elements by referring to their semantic name
+val = document.forms;
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val = document.links; //HTML COLLECTION of the links in current DOM
+val = document.links[0];
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.images; //this will be empty for us, of course
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src'); //app.js
+
+//HTML COLLECTIONS CAN BE TURNED INTO ARRAYS
+//THIS ALLOWS US TO USE ARRAY CLASS METHODS ON THEM
+let scripts = document.scripts;
+let scriptsArray = Array.from(scripts);
+
+scriptsArray.forEach(function(script) {
+	console.log(script);
+});
+console.log(val);

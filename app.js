b3f6a72e9fa5af016b19989dd42bbad07bdddc0d
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-26 01:04:17 -0600

    Basics of functions in JS.

--- app.js
+++ app.js
@@ -1,41 +1,63 @@
-const color = 'purple';
+// FUNCTION DECLARATIONS
+// function greet(firstName, lastName) { //name is a type of parameter, or argument
+// 	//anything in here is the function scope
+// 	return `Hello, ${firstName} ${lastName}!`
+//}
 
-//remember to add the break after each case
-switch(color) {
-	case 'red':
-		console.log('color is red');
-		break;
-	case 'blue':
-		console.log('color is blue');
-		break;
-	default:
-		console.log('color is not red or blue');
-		break;
+//call the function
+// console.log(greet('David', 'Lowe')); //without the passed in parameters, these values are "undefined"
+
+//handling default values, the 'old' way
+function oldGreet(firstName, lastName) {
+	if(typeof firstName === 'undefined'){firstName = 'John'}
+	if(typeof lastName === 'undefined'){lastName = 'Doe'}
+
+	return `Hello, ${firstName} ${lastName}!`
+
 }
 
-let day;
-switch(new Date().getDay()) {
-	case 0:
-		day = 'Sunday';
-		break;
-	case 1:
-		day = 'Monday';
-		break;
-	case 2:
-		day = 'Tuesday';
-		break;
-	case 3:
-		day = 'Wednesday';
-		break;
-	case 4:
-		day = 'Thursday';
-		break;
-	case 5:
-		day = 'Friday';
-		break;
-	case 6:
-		day = 'Saturday';
-		break;
+//handling default the modern way
+function greet(firstName = 'John', lastName = 'Doe') {
+	return `Hello, ${firstName} ${lastName}!`
 }
 
-console.log(`Today is ${day}`);
+//console.log(greet()); // returns Hello, John Doe
+//console.log(greet('David', 'Lowe'));
+
+//Function expressions = seting a function to a variable
+const square = function(x = 3) {
+	return x * x;
+};
+
+//console.log(square()); //returns 9, using the default value of 3
+console.log(square(8)); //64
+
+//IMMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFES
+//A function that is declared and run at the same time
+// (function() {
+// 	console.log('IIFE Ran...');
+// })()
+
+//this is used often in the module pattern, 'a bunch of IIFEs with private properties'
+// (function(name) {
+// 	console.log(`Hello, ${name}`);
+// })('Bradley');
+
+//remember that objects can contain functions - these are called methods
+const todo = {
+	add: function(){
+		console.log('Add todo...');
+	},
+	edit: function(id){
+		console.log(`Edit todo: ${id}`);
+	}
+}
+
+//functions, like other object properties, can also be defined outside of the declaration body of that object
+todo.delete = function() {
+	console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(13);
+todo.delete();
